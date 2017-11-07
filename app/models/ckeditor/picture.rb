5717e90a81ace28268ef336817ee931a8132d2b8
Cayvogikonyo <kevohm.km@gmail.com>
2017-11-07 13:49:12 +0300

    ckeditor

--- app/models/ckeditor/picture.rb
+++ app/models/ckeditor/picture.rb
@@ -1,6 +1,6 @@
 class Ckeditor::Picture < Ckeditor::Asset
   has_attached_file :data,
-                    path: ':rails_root/public/ckeditor_assets/pictures/:id/:style_:basename.:extension',
+                    path: '/pictures/:id/:style_:basename.:extension',
                     styles: { content: '800>', thumb: '118x100#' }
 
   validates_attachment_presence :data
