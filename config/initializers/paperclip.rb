55a9ba3b54f61817f52374adee1b5ddc070f030b
Cayvogikonyo <kevohm.km@gmail.com>
2017-11-07 15:50:46 +0300

    ckeditor

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,3 +1,6 @@
 Paperclip::Attachment.default_options[:url] = 'jogooletters.s3.amazonaws.com'
 Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
 Paperclip::Attachment.default_options[:s3_host_name] = 'jogooletters.s3.amazonaws.com'
+Paperclip.interpolates(:s3_sg_url) do |att, style| 
+"#{att.s3_protocol}://s3-eu-west-2.amazonaws.com/#{att.bucket_name}/#{att.path(style)}"
+end
