efe89630708f7e32b9477d99972abb16a207b1a2
Cayvogikonyo <kevohm.km@gmail.com>
2017-11-08 07:08:25 +0300

    ckeditor

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,2 +1,2 @@
 Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
-Paperclip::Attachment.default_options[:s3_host_name] = 's3.eu-west-2.amazonaws.com/jogooletters'
+Paperclip::Attachment.default_options[:s3_host_name] = 's3.eu-west-2.amazonaws.com'
